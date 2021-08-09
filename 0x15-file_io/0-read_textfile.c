3ea0be1eae25b0f03a4de7af174038e75e85d60f
Alfonsomaclean <alfonsomacleandavis97@gmail.com>
2021-08-09 12:27:04 -0700

    Program that displays the information contained in the ELF header at the start of an ELF file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,35 +1,31 @@
-#include "main.h"
 #include "holberton.h"
-#include <stdlib.h>
 
 /**
- * read_textfile - reads a text file and prints it to the standard output
- * @filename: the file name to be read
- * @letters: number of letters to read and print
- * Return: the number of letters read and print
+ * read_textfile - reads a text file and prints it to standard output
+ * @filename: file descripter
+ * @letters: count
+ *
+ * Return: any time size_t
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int filetxt;
-	ssize_t nrd, nwr;
-	char *buffer;
+	int fd, sz, oz;
+	char *buf = malloc(sizeof(char) * letters);
 
-	if (filename == NULL)
+	fd = open(filename, O_RDONLY);
+	if ((filename == NULL) || (fd == -1) || (buf == NULL))
+	{
+		free(buf);
 		return (0);
-
-	filetxt = open(filename, O_RDONLY);
-	if (filetxt == -1)
+	}
+	sz = read(fd, buf, letters);
+	if (sz == -1)
 		return (0);
-
-	buffer = malloc(sizeof(char) * (letters));
-	if (buffer == NULL)
+	buf[sz] = '\0';
+	oz = write(STDOUT_FILENO, buf, sz);
+	if (oz == -1)
 		return (0);
-
-	nrd = read(filetxt, buffer, letters);
-	nwr = write(STDOUT_FILENO, buffer, nrd);
-
-	close(filetxt);
-	free(buffer);
-	return (nwr);
-
+	close(fd);
+	return (oz);
 }
