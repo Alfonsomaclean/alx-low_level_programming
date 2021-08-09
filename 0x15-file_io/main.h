8ebe3b89dfe368338c7fb7eb16b16ab6ad1c4c5c
Alfonsomaclean <alfonsomacleandavis97@gmail.com>
2021-08-09 12:32:28 -0700

    Header File

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,21 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <stdio.h>
 #include <unistd.h>
-#include <errno.h>
-#include <stdlib.h>
-#include "holberton.h"
 
 int _putchar(char c);
-
 ssize_t read_textfile(const char *filename, size_t letters);
-
-int create_file(const char *filename, char *text_content);
-
 int append_text_to_file(const char *filename, char *text_content);
+int create_file(const char *filename, char *text_content);
+int _strlen(char *s);
+void close_new(int f_n);
 
 #endif
